5dfed070d8ab11f472856242cf0c5df46f80b322
Aurasplash <aurasplash@gmail.com>
2020-12-31 11:58:28 -0700

    Basic Settings
    Basic Settings

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -30,17 +30,17 @@ ENABLE_VOIDWATCH = 1 -- Not an expansion, but has its own storyline.
 -- FIELDS OF VALOR/Grounds of Valor settings
 ENABLE_FIELD_MANUALS  = 1 -- Enables Fields of Valor
 ENABLE_GROUNDS_TOMES  = 1 -- Enables Grounds of Valor
-ENABLE_SURVIVAL_GUIDE = 1 -- Enables Survival Guides
+ENABLE_SURVIVAL_GUIDE = 0 -- Enables Survival Guides
 REGIME_WAIT = 1 -- Make people wait till 00:00 game time as in retail. If it's 0, there is no wait time.
 FOV_REWARD_ALLIANCE = 0 -- Allow Fields of Valor rewards while being a member of an alliance. (default retail behavior: 0)
-GOV_REWARD_ALLIANCE = 1 -- Allow Grounds of Valor rewards while being a member of an alliance. (default retail behavior: 1)
+GOV_REWARD_ALLIANCE = 0 -- Allow Grounds of Valor rewards while being a member of an alliance. (default retail behavior: 1)
 
 -- Records of Eminence
-ENABLE_ROE = 1
-ENABLE_ROE_TIMED = 1 -- Enable 4-hour timed records
+ENABLE_ROE = 0
+ENABLE_ROE_TIMED = 0 -- Enable 4-hour timed records
 
 -- Magian Trials
-ENABLE_MAGIAN_TRIALS = 1
+ENABLE_MAGIAN_TRIALS = 0
 
 -- TREASURE CASKETS
 -- Retail droprate = 0.1 (10%) with no other effects active
@@ -83,7 +83,7 @@ DARK_POWER      = 1.000 -- Multiplies amount drained by Dark Magic.
 ITEM_POWER      = 1.000 -- Multiplies the effect of items such as Potions and Ethers.
 WEAPON_SKILL_POWER  = 1.000 -- Multiplies damage dealt by Weapon Skills.
 WEAPON_SKILL_POINTS = 1.000 -- Multiplies points earned during weapon unlocking.
-USE_ADOULIN_WEAPON_SKILL_CHANGES = true -- true/false. Change to toggle new Adoulin weapon skill damage calculations
+USE_ADOULIN_WEAPON_SKILL_CHANGES = false -- true/false. Change to toggle new Adoulin weapon skill damage calculations
 
 -- TRUSTS
 ENABLE_TRUST_CASTING = 0
@@ -110,8 +110,8 @@ CHEST_MIN_ILLUSION_TIME  = 1800  -- 30 minutes
 
 -- Sets spawn type for: Behemoth, Fafnir, Adamantoise, King Behemoth, Nidhog, Aspidochelone.
 -- Use 0 for timed spawns, 1 for force pop only, 2 for both
-LandKingSystem_NQ = 1
-LandKingSystem_HQ = 1
+LandKingSystem_NQ = 0
+LandKingSystem_HQ = 0
 
 -- Multiplier to NM lottery spawn chance. (Default 1.0) eg. 0 = disable lottery spawns. -1 for always 100% chance.
 NM_LOTTERY_CHANCE = 1.0
@@ -119,8 +119,8 @@ NM_LOTTERY_CHANCE = 1.0
 NM_LOTTERY_COOLDOWN = 1.0
 
 -- DYNAMIS SETTINGS
-BETWEEN_2DYNA_WAIT_TIME  = 24       -- Hours before player can re-enter Dynamis. Default is 1 Earthday (24 hours).
-DYNA_MIDNIGHT_RESET      = true     -- If true, makes the wait time count by number of server midnights instead of full 24 hour intervals
+BETWEEN_2DYNA_WAIT_TIME  = 72       -- Hours before player can re-enter Dynamis. Default is 1 Earthday (24 hours).
+DYNA_MIDNIGHT_RESET      = false     -- If true, makes the wait time count by number of server midnights instead of full 24 hour intervals
 DYNA_LEVEL_MIN           = 65       -- Level min for entering in Dynamis
 TIMELESS_HOURGLASS_COST  = 500000   -- Refund for the timeless hourglass for Dynamis.
 PRISMATIC_HOURGLASS_COST = 50000    -- Cost of the prismatic hourglass for Dynamis.
@@ -133,8 +133,8 @@ FREE_COP_DYNAMIS = 0 -- Authorize player to entering inside COP Dynamis without 
 AF1_QUEST_LEVEL = 40    -- Minimum level to start AF1 quest
 AF2_QUEST_LEVEL = 50    -- Minimum level to start AF2 quest
 AF3_QUEST_LEVEL = 50    -- Minimum level to start AF3 quest
-OLDSCHOOL_G1    = false -- Set to true to require farming Exoray Mold, Bombd Coal, and Ancient Papyrus drops instead of allowing key item method.
-OLDSCHOOL_G2    = false -- Set true to require the NMs for "Atop the Highest Mountains" be dead to get KI like before SE changed it.
+OLDSCHOOL_G1    = true -- Set to true to require farming Exoray Mold, Bombd Coal, and Ancient Papyrus drops instead of allowing key item method.
+OLDSCHOOL_G2    = true -- Set true to require the NMs for "Atop the Highest Mountains" be dead to get KI like before SE changed it.
 FRIGICITE_TIME  = 30    -- When OLDSCHOOL_G2 is enabled, this is the time (in seconds) you have from killing Boreal NMs to click the "???" target.
 
 -- SPELL SPECIFIC SETTINGS
@@ -148,11 +148,11 @@ AQUAVEIL_COUNTER = 1  -- Base amount of hits Aquaveil absorbs to prevent spell i
 ABSORB_SPELL_AMOUNT = 8 -- how much of a stat gets absorbed by DRK absorb spells - expected to be a multiple of 8.
 ABSORB_SPELL_TICK = 9 -- duration of 1 absorb spell tick
 SNEAK_INVIS_DURATION_MULTIPLIER = 1 -- multiplies duration of sneak, invis, deodorize to reduce player torture. 1 = retail behavior.
-USE_OLD_CURE_FORMULA = false -- true/false. if true, uses older cure formula (3*MND + VIT + 3*(healing skill/5)) // cure 6 will use the newer formula
-USE_OLD_MAGIC_DAMAGE = false -- true/false. if true, uses older magic damage formulas
+USE_OLD_CURE_FORMULA = true -- true/false. if true, uses older cure formula (3*MND + VIT + 3*(healing skill/5)) // cure 6 will use the newer formula
+USE_OLD_MAGIC_DAMAGE = true -- true/false. if true, uses older magic damage formulas
 
 -- CELEBRATIONS
-EXPLORER_MOOGLE_LV = 10 -- Enables Explorer Moogle teleports and sets required level. Zero to disable.
+EXPLORER_MOOGLE_LV = 0 -- Enables Explorer Moogle teleports and sets required level. Zero to disable.
 HALLOWEEN_2005 = 0 -- Set to 1 to Enable the 2005 version of Harvest Festival, will start on Oct. 20 and end Nov. 1.
 HALLOWEEN_YEAR_ROUND = 0 -- Set to 1 to have Harvest Festival initialize outside of normal times.
 
@@ -160,7 +160,7 @@ HALLOWEEN_YEAR_ROUND = 0 -- Set to 1 to have Harvest Festival initialize outside
 HOMEPOINT_HEAL = 0 --Set to 1 if you want Home Points to heal you like in single-player Final Fantasy games.
 RIVERNE_PORTERS = 120 -- Time in seconds that Unstable Displacements in Cape Riverne stay open after trading a scale.
 LANTERNS_STAY_LIT = 1200 -- time in seconds that lanterns in the Den of Rancor stay lit.
-ENABLE_COP_ZONE_CAP = 0 -- enable or disable lvl cap
+ENABLE_COP_ZONE_CAP = 1 -- enable or disable lvl cap
 ALLOW_MULTIPLE_EXP_RINGS = 0 -- Set to 1 to remove ownership restrictions on the Chariot/Empress/Emperor Band trio.
 BYPASS_EXP_RING_ONE_PER_WEEK = 0 -- -- Set to 1 to bypass the limit of one ring per Conquest Tally Week.
 NUMBER_OF_DM_EARRINGS = 1 -- Number of earrings players can simultaneously own from Divine Might before scripts start blocking them (Default: 1)
